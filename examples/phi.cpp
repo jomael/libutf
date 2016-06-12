d404c2b9dd806dae052436e0c2cf7ba5516a3e59
Taylor Holberton <taylorcholberton@gmail.com>
2016-06-12 13:30:29 -0400

    added integer literal for msvc

--- examples/phi.cpp
+++ examples/phi.cpp
@@ -2,13 +2,22 @@
 
 #include <iostream>
 
+#ifdef _MSC_VER
+#pragma warning(disable : 4710)
+#pragma warning(disable : 4514)
+#endif /* _MSC_VER */
+
 int main(void){
 
 	unsigned char buffer[4] = { 0, 0, 0, 0 };
 
 	utfx::Encoder encoder(utfx::Encoder::Mode::UTF8);
 
+#ifdef _MSC_VER
+	encoder.Write(0x03a6);
+#else /* _MSC_VER */
 	encoder.Write(U'Φ');
+#endif /* _MSC_VER */
 
 	auto read_count = encoder.Read(buffer, sizeof(buffer));
 
