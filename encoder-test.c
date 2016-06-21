829c4b4cf97d51816627b37493ff3dec5160a119
Taylor Holberton <taylorcholberton@gmail.com>
2016-06-20 21:41:27 -0400

    Added another test

--- encoder-test.c
+++ encoder-test.c
@@ -4,8 +4,11 @@
 
 static void test_utf8(void);
 
+static void test_utf16be(void);
+
 int main(void){
 	test_utf8();
+	test_utf16be();
 	return 0;
 }
 
@@ -51,4 +54,42 @@ static void test_utf8(void){
 	/* should be back in a reading state */
 	assert(utfx_encoder_get_state(&encoder) == UTFX_ENCODER_STATE_READING);
 } /* namespace */
+
+static void test_utf16be(void){
+
+	unsigned int read_count = 0;
+
+	unsigned char byte = 0;
+
+	utfx_error_t error = UTFX_ERROR_NONE;
+
+	utfx_encoder_t encoder;
+
+	utfx_encoder_init(&encoder);
+
+	utfx_encoder_set_mode(&encoder, UTFX_ENCODER_MODE_UTF16_BE);
+
+	/* UTF-32BE : 00 02 4b 62 */
+	/* UTF-16BE : d8 52 df 62 */
+	error = utfx_encoder_write(&encoder, 0x00024b62);
+	assert(error == UTFX_ERROR_NONE);
+
+	assert(utfx_encoder_get_state(&encoder) == UTFX_ENCODER_STATE_WRITING);
+
+	read_count = utfx_encoder_read(&encoder, &byte, 1);
+	assert(read_count == 1);
+	assert(byte == 0xd8);
+
+	read_count = utfx_encoder_read(&encoder, &byte, 1);
+	assert(read_count == 1);
+	assert(byte == 0x52);
+
+	read_count = utfx_encoder_read(&encoder, &byte, 1);
+	assert(read_count == 1);
+	assert(byte == 0xdf);
+
+	read_count = utfx_encoder_read(&encoder, &byte, 1);
+	assert(read_count == 1);
+	assert(byte == 0x62);
+}
 
