dbedfa9d894e165ec24eb3efa4b7f5f6e320ee0c
Taylor Holberton <taylorcholberton@gmail.com>
2017-01-01 13:37:36 -0800

    added test_insert()

--- src/string-test.c
+++ src/string-test.c
@@ -4,8 +4,11 @@
 
 static void test_compare(void);
 
+static void test_insert(void);
+
 int main(void){
 	test_compare();
+	test_insert();
 	return 0;
 }
 
@@ -35,5 +38,23 @@ static void test_compare(void){
 
 	utf_string_free(&a);
 	utf_string_free(&b);
+}
+
+static void test_insert(void){
+
+	utf_error_t error;
+	utf_string_t string;
+
+	utf_string_init(&string);
+
+	error = utf_string_copy_asciiz(&string, "Hello, !");
+	assert(error == UTF_ERROR_NONE);
+
+	error = utf_string_insert_asciiz(&string, "Bill", 7);
+	assert(error == UTF_ERROR_NONE);
+
+	assert(utf_string_compare_asciiz(&string, "Hello, Bill!") == 0);
+
+	utf_string_free(&string);
 }
 
