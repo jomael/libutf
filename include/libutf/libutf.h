13ce4972dc139e2b5302a58d9c9c258e8139560b
Taylor Holberton <taylorcholberton@gmail.com>
2017-01-01 13:49:57 -0800

    added new headers

--- include/libutf/libutf.h
+++ include/libutf/libutf.h
@@ -1,17 +1,21 @@
 #ifndef LIBUTF_H
 #define LIBUTF_H
 
-#include "version.h"
-
+#include "converter.h"
+#include "decoder.h"
+#include "encoder.h"
 #include "error.h"
-
+#include "ifstream.h"
+#include "istream.h"
+#include "iterator.h"
+#include "ofstream.h"
+#include "ostream.h"
+#include "stream.h"
+#include "string.h"
 #include "utf8.h"
 #include "utf16.h"
 #include "utf32.h"
-
-#include "encoder.h"
-#include "decoder.h"
-#include "converter.h"
+#include "version.h"
 
 #endif /* LIBUTF_H */
 
