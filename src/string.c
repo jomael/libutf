273c4d419e1fb64ecb3d270163bf91994f84ab9e
Taylor Holberton <taylorcholberton@gmail.com>
2017-01-01 16:12:30 -0800

    implemented _insert()

--- src/string.c
+++ src/string.c
@@ -18,24 +18,44 @@
 #include <libutf/string.h>
 #include <libutf/iterator.h>
 
+#include <libutf/utf8.h>
+#include <libutf/utf16.h>
+
 #include <stdlib.h>
 #include <string.h>
 
 void utf_string_init(utf_string_t * string){
 	string->bits = 8;
 	string->count = 0;
-	string->count_res = 0;
-	string->u.u8 = NULL;
+	string->reserved = 0;
+	string->data.u8 = NULL;
+	string->data_const.u8 = NULL;
 }
 
 void utf_string_free(utf_string_t * string){
 	if (string != NULL){
-		free(string->u.u8);
+		free(string->data.u8);
 	}
 }
 
+void utf_string_assign_ascii(utf_string_t * string, const char * ascii, utf_byte_count_t ascii_len){
+	string->bits = 8;
+	string->count = ascii_len;
+	string->reserved = ascii_len;
+	string->data.u8 = NULL;
+	string->data_const.u8 = (const utf8_t *)(ascii);
+}
+
+void utf_string_assign_utf8(utf_string_t * string, const utf8_t * src, utf_unit_count_t src_len){
+	string->bits = 8;
+	string->count = src_len;
+	string->reserved = src_len;
+	string->data.u8 = NULL;
+	string->data_const.u8 = src;
+}
+
 unsigned long int utf_string_avail(const utf_string_t * string){
-	return string->count_res - string->count;
+	return string->reserved - string->count;
 }
 
 utf_byte_count_t utf_string_byte_count(const utf_string_t * string){
@@ -97,14 +117,24 @@ int utf_string_compare(const utf_string_t * a, const utf_string_t * b){
 	return 0;
 }
 
+int utf_string_compare_ascii(const utf_string_t * a, const char * b, utf_byte_count_t b_len){
+	utf_string_t b2;
+	utf_string_assign_ascii(&b2, b, b_len);
+	return utf_string_compare(a, &b2);
+}
+
+int utf_string_compare_asciiz(const utf_string_t * a, const char * b){
+	return utf_string_compare_ascii(a, b, strlen(b));
+}
+
 utf_error_t utf_string_copy(utf_string_t * dst, const utf_string_t * src){
 	unsigned long int size = 0;
-	if (dst->u.u8 != NULL){
-		free(dst->u.u8);
+	if (dst->data.u8 != NULL){
+		free(dst->data.u8);
 	}
 	dst->bits = src->bits;
 	dst->count = src->count;
-	dst->count_res = src->count_res;
+	dst->reserved = src->reserved;
 	if (dst->bits == 8){
 		size = dst->count;
 	} else if (dst->bits == 16){
@@ -112,30 +142,38 @@ utf_error_t utf_string_copy(utf_string_t * dst, const utf_string_t * src){
 	} else if (dst->bits == 32){
 		size = dst->count * 4;
 	}
-	dst->u.u8 = malloc(size);
-	if ((dst->u.u8 == NULL) && (size > 0)){
+	dst->data.u8 = malloc(size);
+	if ((dst->data.u8 == NULL) && (size > 0)){
 		return UTF_ERROR_MALLOC;
 	}
-	memcpy(dst->u.u8, src->u.u8, size);
+	dst->data_const.u8 = dst->data.u8;
+	memcpy(dst->data.u8, src->data_const.u8, size);
 	return UTF_ERROR_NONE;
+}
+
+utf_error_t utf_string_copy_ascii(utf_string_t * dst, const char * src, utf_byte_count_t src_len){
+	utf_string_t src2;
+	utf_string_assign_ascii(&src2, src, src_len);
+	return utf_string_copy(dst, &src2);
+}
+
+utf_error_t utf_string_copy_asciiz(utf_string_t * dst, const char * asciiz){
+	return utf_string_copy_ascii(dst, asciiz, strlen(asciiz));
 }
 
 utf_error_t utf_string_copy_utf8(utf_string_t * dst, const utf8_t * src, unsigned long int src_len){
-	if (dst->u.u8 != NULL){
-		free(dst->u.u8);
-	}
-	dst->bits = 8;
-	dst->count = src_len;
-	dst->count_res = 0;
-	dst->u.u8 = malloc(src_len);
-	if (dst->u.u8 == NULL){
-		return UTF_ERROR_MALLOC;
-	}
-	memcpy(dst->u.u8, src, src_len);
-	return UTF_ERROR_NONE;
+	utf_string_t src2;
+	utf_string_assign_utf8(&src2, src, src_len);
+	return utf_string_copy(dst, &src2);
 }
 
 utf_error_t utf_string_insert(utf_string_t * dst, const utf_string_t * src, utf_unit_index_t index){
+
+	utf_byte_index_t byte_index;
+	utf_byte_count_t dst_size;
+	utf_byte_count_t src_size;
+	utf_unit_count_t i;
+	utf_unit_count_t j;
 
 	if (index > dst->count){
 		return UTF_ERROR_BOUNDARY;
@@ -143,13 +181,64 @@ utf_error_t utf_string_insert(utf_string_t * dst, const utf_string_t * src, utf_
 
 	if (utf_string_avail(dst) < src->count){
 		utf_error_t error;
-		error = utf_string_reserve(dst, src->count);
+		error = utf_string_reserve(dst, dst->count + src->count);
 		if (error){
 			return error;
 		}
 	}
 
+	byte_index = (index * dst->bits) / 8;
+	dst_size = utf_string_byte_count(dst);
+	src_size = utf_string_byte_count(src);
+
+	memmove(&dst->data.u8[byte_index + src_size],
+	        &dst->data.u8[byte_index],
+	        dst_size - byte_index);
+
+	i = 0;
+	while (i < src->count){
+
+		utf32_t ch = 0;
+
+		if (src->bits == 8){
+			j = utf8_decode(&src->data_const.u8[i], &ch);
+			if (j == 0){
+				return UTF_ERROR_INVALID_SEQUENCE;
+			}
+		} else if (src->bits == 16){
+			j = utf16_decode(&src->data_const.u16[i], &ch);
+			if (j == 0){
+				return UTF_ERROR_INVALID_SEQUENCE;
+			}
+		} else if (src->bits == 32){
+			ch = src->data_const.u32[i];
+			j = 1;
+		}
+
+		if (dst->bits == 8){
+			utf8_encode(ch, &dst->data.u8[index + i]);
+		} else if (dst->bits == 16){
+			utf16_encode(ch, &dst->data.u16[index + i]);
+		} else if (dst->bits == 32){
+			dst->data.u32[index + i] = ch;
+		}
+
+		i += j;
+	}
+
+	dst->count += i;
+
 	return UTF_ERROR_NONE;
+}
+
+utf_error_t utf_string_insert_ascii(utf_string_t * dst, const char * ascii, utf_byte_count_t ascii_len, utf_unit_index_t index){
+	utf_string_t src;
+	utf_string_assign_ascii(&src, ascii, ascii_len);
+	return utf_string_insert(dst, &src, index);
+}
+
+utf_error_t utf_string_insert_asciiz(utf_string_t * dst, const char * asciiz, utf_unit_index_t index){
+	return utf_string_insert_ascii(dst, asciiz, strlen(asciiz), index);
 }
 
 utf_error_t utf_string_reserve(utf_string_t * string, unsigned long int count){
@@ -159,13 +248,14 @@ utf_error_t utf_string_reserve(utf_string_t * string, unsigned long int count){
 
 	size = (count * string->bits) / 8;
 
-	tmp = realloc(string->u.u8, size);
+	tmp = realloc(string->data.u8, size);
 	if ((tmp == NULL) && (count > 0)){
 		return UTF_ERROR_MALLOC;
 	}
 
-	string->u.u8 = tmp;
-	string->count_res = count;
+	string->data.u8 = tmp;
+	string->data_const.u8 = tmp;
+	string->reserved = count;
 	if (count < string->count){
 		string->count = count;
 	}
