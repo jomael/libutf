71cee91f9d1bf7ac66bbfe3377f260c944ad41b3
Taylor Holberton <taylorcholberton@gmail.com>
2017-01-01 20:27:37 -0800

    added read functions

--- include/libutf/ifstream.h
+++ include/libutf/ifstream.h
@@ -13,5 +13,11 @@ void utf_ifstream_free(utf_ifstream_t * ifstream);
 
 utf_error_t utf_ifstream_open(utf_ifstream_t * ifstream, const char * path);
 
+utf_unit_count_t utf_ifstream_read_utf8(utf_ifstream_t * ifstream, utf8_t * data, utf_unit_count_t data_max);
+
+utf_unit_count_t utf_ifstream_read_utf16(utf_ifstream_t * ifstream, utf8_t * data, utf_unit_count_t data_max);
+
+utf_unit_count_t utf_ifstream_read_utf32(utf_ifstream_t * ifstream, utf8_t * data, utf_unit_count_t data_max);
+
 #endif /* LIBUTF_IFSTREAM_H */
 
