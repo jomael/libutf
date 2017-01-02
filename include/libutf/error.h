1beac41a6801457907c8b191166d48a5a6e56462
Taylor Holberton <taylorcholberton@gmail.com>
2017-01-02 10:17:12 -0800

    added more error codes

--- include/libutf/error.h
+++ include/libutf/error.h
@@ -25,6 +25,10 @@
 typedef enum utf_error {
 	/** No error occured. */
 	UTF_ERROR_NONE = 0,
+	/** Permission denied */
+	UTF_ERROR_ACCESS,
+	/** Path refers to directory */
+	UTF_ERROR_IS_DIRECTORY,
 	/** Invalid UTF-8, UTF-16 or UTF-32 sequence. */
 	UTF_ERROR_INVALID_SEQUENCE,
 	/** The size of a input or output buffer was not large enough to fit a UTF-8, UTF-16 or UTF-32 sequence. */
@@ -35,6 +39,8 @@ typedef enum utf_error {
 	UTF_ERROR_EOF,
 	/** Attempt to read/write an out of boundary location */
 	UTF_ERROR_BOUNDARY,
+	/** No file or directory exists */
+	UTF_ERROR_MISSING_ENTRY,
 	/** An unknown error occured. */
 	UTF_ERROR_UNKNOWN
 } utf_error_t;
@@ -42,6 +48,16 @@ typedef enum utf_error {
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
+
+/** Returns an error code translated from errno.
+ * @param errno_ An error number set by a library call.
+ * @returns A libutf corresponding error code.
+ *  If no such translation is available,
+ *  then @ref UTF_ERROR_UNKNOWN is returned.
+ * @ingroup libutf
+ */
+
+utf_error_t utf_errno(int errno_);
 
 /** Returns a human-readable string containing a description of the error code.
  * @param error An error code returned from a libutf function call.
--- src/error.c
+++ src/error.c
@@ -16,11 +16,34 @@
  */
 
 #include <libutf/error.h>
+#include <errno.h>
+
+utf_error_t utf_errno(int errno_){
+	switch (errno_){
+		case 0:
+			return UTF_ERROR_NONE;
+		case EACCES:
+			return UTF_ERROR_ACCESS;
+		case ENOENT:
+			return UTF_ERROR_MISSING_ENTRY;
+		case EISDIR:
+			return UTF_ERROR_IS_DIRECTORY;
+		default:
+			break;
+	}
+	return UTF_ERROR_UNKNOWN;
+}
 
 const char * utf_strerror(utf_error_t error){
 	switch (error){
 		case UTF_ERROR_NONE:
 			return "No error";
+		case UTF_ERROR_ACCESS:
+			return "Permission denied";
+		case UTF_ERROR_MISSING_ENTRY:
+			return "No such file or directory";
+		case UTF_ERROR_IS_DIRECTORY:
+			return "Path refers to a directory";
 		case UTF_ERROR_INVALID_SEQUENCE:
 			return "Invalid sequence";
 		case UTF_ERROR_OVERFLOW:
