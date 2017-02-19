5da96c3e20308782f7270381690688568b91a7e3
Taylor Holberton <taylorcholberton@gmail.com>
2017-02-19 10:04:57 -0800

    fixed header guard, added utf_stdout and utf_stderr

--- include/libutf/ofstream.h
+++ include/libutf/ofstream.h
@@ -1,10 +1,10 @@
-#ifndef LIBUTF_IFSTREAM_H
-#define LIBUTF_IFSTREAM_H
+#ifndef LIBUTF_OFSTREAM_H
+#define LIBUTF_OFSTREAM_H
 
 #include <libutf/ostream.h>
 
 typedef struct utf_ofstream {
-	utf_istream_t istream;
+	utf_ostream_t ostream;
 } utf_ofstream_t;
 
 void utf_ofstream_init(utf_ofstream_t * ofstream);
@@ -13,5 +13,21 @@ void utf_ofstream_free(utf_ofstream_t * ofstream);
 
 utf_error_t utf_ofstream_open(utf_ofstream_t * ofstream, const char * path);
 
-#endif /* LIBUTF_IFSTREAM_H */
+utf_unit_count_t utf_ofstream_write(utf_ofstream_t * ofstream, const utf_string_t * string);
+
+utf_unit_count_t utf_ofstream_write_asciiz(utf_ofstream_t * ofstream, const char * asciiz_str);
+
+/** The UTF stdout equivalent */
+
+extern utf_ofstream_t utf_stdout;
+
+void utf_stdout_init(utf_ofstream_t * ofstream);
+
+/** The UTF stderr equivalent */
+
+extern utf_ofstream_t utf_stderr;
+
+void utf_stderr_init(utf_ofstream_t * ofstream);
+
+#endif /* LIBUTF_OFSTREAM_H */
 
