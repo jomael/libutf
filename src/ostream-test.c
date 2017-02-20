7d2e9cdab3df8183523b2ae78433160c6717197a
Taylor Holberton <taylorcholberton@gmail.com>
2017-02-19 18:02:07 -0800

    added ostream-test

---
+++ src/ostream-test.c
@@ -0,0 +1,52 @@
+#include <libutf/ostream.h>
+
+#include <assert.h>
+#include <stdlib.h>
+
+struct ostream_data {
+	const char * expected_str;
+	size_t i;
+};
+
+static utf_unit_count_t write_cb(void * stream_data_ptr, const void * chars_ptr, size_t chars_len){
+
+	struct ostream_data * data;
+	const utf8_t * chars;
+	size_t j;
+
+	data = (struct ostream_data *)(stream_data_ptr);
+	chars = (const utf8_t *)(chars_ptr);
+
+	for (j = 0; j < chars_len; j++){
+		assert(data->expected_str[data->i] == chars[j]);
+		data->i++;
+	}
+
+	return chars_len;
+}
+
+static void test_write(void){
+
+	utf_string_t string;
+	utf_ostream_t ostream;
+	struct ostream_data data;
+
+	data.expected_str = "hello, world!";
+	data.i = 0;
+
+	utf_ostream_init(&ostream);
+	utf_ostream_set_data(&ostream, &data);
+	utf_ostream_set_write(&ostream, write_cb);
+
+	utf_string_init(&string);
+	string.count = sizeof("hello, world!") - 1;
+	string.data_const.u8 = "hello, world!";
+
+	assert(utf_ostream_write(&ostream, &string) == 13);
+}
+
+int main(void){
+	test_write();
+	return EXIT_SUCCESS;
+}
+
