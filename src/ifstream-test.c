666942e4c29fc10f2209b19da260041a1a1b1268
Taylor Holberton <taylorcholberton@gmail.com>
2017-02-20 20:19:35 -0500

    removed variables not needed

--- src/ifstream-test.c
+++ src/ifstream-test.c
@@ -10,18 +10,14 @@ int main(void){
 
 void test_utf8(void){
 
-	utf_error_t error;
-	utf_ifstream_t ifstream;
-	utf32_t ch;
-	utf_unit_count_t read_count;
+	struct utf_ifstream ifstream;
+	char32_t ch;
 
 	utf_ifstream_init(&ifstream);
 
-	error = utf_ifstream_open(&ifstream, "../testing/arabic/UTF-8.txt");
-	assert(error == UTF_ERROR_NONE);
+	assert(utf_ifstream_open(&ifstream, "../testing/arabic/UTF-8.txt") == 0);
 
-	read_count = utf_ifstream_read_utf32(&ifstream, &ch, 1);
-	assert(read_count == 1);
+	assert(utf_ifstream_read_utf32(&ifstream, &ch, 1) == 1);
 
 	utf_ifstream_free(&ifstream);
 }
