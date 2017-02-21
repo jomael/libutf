ec4ea9e7525cfa6ca7bfd345e8c4dbe11960eca5
Taylor Holberton <taylorcholberton@gmail.com>
2017-02-20 20:20:05 -0500

    removing utf_istream_t typedef

--- src/istream.c
+++ src/istream.c
@@ -3,59 +3,59 @@
 
 #include <stdlib.h>
 
-static utf_byte_count_t utf_istream_read_any(utf_istream_t * istream,
-                                             unsigned char * bytes,
-                                             utf_byte_count_t data_max,
-                                             utf_codec_t dst_codec);
+static size_t utf_istream_read_any(struct utf_istream * istream, unsigned char * bytes, size_t data_max, utf_codec_t dst_codec);
 
-void utf_istream_init(utf_istream_t * istream){
+void utf_istream_init(struct utf_istream * istream){
 	utf_stream_init(&istream->stream);
 	istream->read_cb = NULL;
 }
 
-void utf_istream_free(utf_istream_t * istream){
+void utf_istream_free(struct utf_istream * istream){
 	utf_stream_free(&istream->stream);
 }
 
-utf_codec_t utf_istream_get_codec(const utf_istream_t * istream){
+utf_codec_t utf_istream_get_codec(const struct utf_istream * istream){
 	return utf_stream_get_codec(&istream->stream);
 }
 
-void utf_istream_set_codec(utf_istream_t * istream, utf_codec_t codec){
+void utf_istream_set_codec(struct utf_istream * istream, utf_codec_t codec){
 	utf_stream_set_codec(&istream->stream, codec);
 }
 
-void utf_istream_set_data(utf_istream_t * istream, void * data){
+void utf_istream_set_data(struct utf_istream * istream, void * data){
 	utf_stream_set_data(&istream->stream, data);
 }
 
-void utf_istream_set_free(utf_istream_t * istream, utf_free_callback_t free_cb){
+void utf_istream_set_free(struct utf_istream * istream, utf_free_callback_t free_cb){
 	utf_stream_set_free(&istream->stream, free_cb);
 }
 
-void utf_istream_set_read(utf_istream_t * istream, utf_read_callback_t read_cb){
+void utf_istream_set_read(struct utf_istream * istream, utf_read_callback_t read_cb){
 	istream->read_cb = read_cb;
 }
 
-utf_unit_count_t utf_istream_read_utf8(utf_istream_t * istream, utf8_t * data, utf_unit_count_t data_max){
-	return utf_istream_read_any(istream, (unsigned char *)(data), data_max * sizeof(utf8_t), UTF_CODEC_UTF8);
+size_t utf_istream_read(struct utf_istream * istream, struct utf_string * string){
+	(void) istream;
+	(void) string;
+	return 0;
 }
 
-utf_unit_count_t utf_istream_read_utf16(utf_istream_t * istream, utf16_t * data, utf_unit_count_t data_max){
-	return utf_istream_read_any(istream, (unsigned char *)(data), data_max * sizeof(utf16_t), UTF_CODEC_UTF16) / 2;
+size_t utf_istream_read_utf8(struct utf_istream * istream, char * data, size_t data_max){
+	return utf_istream_read_any(istream, (unsigned char *)(data), data_max * sizeof(char), UTF_CODEC_UTF8);
 }
 
-utf_unit_count_t utf_istream_read_utf32(utf_istream_t * istream, utf32_t * data, utf_unit_count_t data_max){
-	return utf_istream_read_any(istream, (unsigned char *)(data), data_max * sizeof(utf16_t), UTF_CODEC_UTF32) / 4;
+size_t utf_istream_read_utf16(struct utf_istream * istream, char16_t * data, size_t data_max){
+	return utf_istream_read_any(istream, (unsigned char *)(data), data_max * sizeof(char16_t), UTF_CODEC_UTF16) / 2;
 }
 
-static utf_byte_count_t utf_istream_read_any(utf_istream_t * istream,
-                                             unsigned char * bytes,
-                                             utf_byte_count_t data_max,
-                                             utf_codec_t dst_codec){
-	utf_unit_count_t i;
-	utf_byte_count_t write_count;
-	utf_byte_count_t read_count;
+size_t utf_istream_read_utf32(struct utf_istream * istream, char32_t * data, size_t data_max){
+	return utf_istream_read_any(istream, (unsigned char *)(data), data_max * sizeof(char16_t), UTF_CODEC_UTF32) / 4;
+}
+
+static size_t utf_istream_read_any(struct utf_istream * istream, unsigned char * bytes, size_t data_max, utf_codec_t dst_codec){
+	size_t i;
+	size_t write_count;
+	size_t read_count;
 	utf_converter_t converter;
 	unsigned char byte;
 
