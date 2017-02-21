fd33b0e3715b2953fe4eda72147c2ffa650cf0cb
Taylor Holberton <taylorcholberton@gmail.com>
2017-02-20 20:15:26 -0500

    removing typedef

--- include/libutf/ifstream.h
+++ include/libutf/ifstream.h
@@ -3,37 +3,37 @@
 
 #include <libutf/istream.h>
 
-typedef struct utf_ifstream {
-	utf_istream_t istream;
-} utf_ifstream_t;
+struct utf_ifstream {
+	struct utf_istream istream;
+};
 
-void utf_ifstream_init(utf_ifstream_t * ifstream);
+void utf_ifstream_init(struct utf_ifstream * ifstream);
 
-void utf_ifstream_free(utf_ifstream_t * ifstream);
+void utf_ifstream_free(struct utf_ifstream * ifstream);
 
-utf_codec_t utf_ifstream_get_codec(const utf_ifstream_t * ifstream);
+utf_codec_t utf_ifstream_get_codec(const struct utf_ifstream * ifstream);
 
-utf_error_t utf_ifstream_open(utf_ifstream_t * ifstream, const char * path);
+int utf_ifstream_open(struct utf_ifstream * ifstream, const char * path);
 
-utf_unit_count_t utf_ifstream_read_utf8(utf_ifstream_t * ifstream, utf8_t * data, utf_unit_count_t data_max);
+size_t utf_ifstream_read_utf8(struct utf_ifstream * ifstream, char * data, size_t data_max);
 
-utf_unit_count_t utf_ifstream_read_utf16(utf_ifstream_t * ifstream, utf16_t * data, utf_unit_count_t data_max);
+size_t utf_ifstream_read_utf16(struct utf_ifstream * ifstream, char16_t * data, size_t data_max);
 
-utf_unit_count_t utf_ifstream_read_utf32(utf_ifstream_t * ifstream, utf32_t * data, utf_unit_count_t data_max);
+size_t utf_ifstream_read_utf32(struct utf_ifstream * ifstream, char32_t * data, size_t data_max);
 
-void utf_ifstream_set_codec(utf_ifstream_t * ifstream, utf_codec_t codec);
+void utf_ifstream_set_codec(struct utf_ifstream * ifstream, utf_codec_t codec);
 
 /** The UTF version of the stdin handle.
  */
 
-extern utf_ifstream_t utf_stdin;
+extern struct utf_ifstream utf_stdin;
 
 /** Initializes a input file stream with an stdin handle.
  * @param ifstream The structure to initialize the stdin handle with.
  *  If this parameter is NULL, it initializes @ref utf_stdin.
  */
 
-void utf_stdin_init(utf_ifstream_t * ifstream);
+void utf_stdin_init(struct utf_ifstream * ifstream);
 
 #endif /* LIBUTF_IFSTREAM_H */
 
--- include/libutf/istream.h
+++ include/libutf/istream.h
@@ -1,42 +1,46 @@
 #ifndef LIBUTF_ISTREAM_H
 #define LIBUTF_ISTREAM_H
 
+/* for char16_t and char32_t */
+#include <uchar.h>
+
 #include <libutf/stream.h>
+#include <libutf/string.h>
 
-typedef struct utf_istream utf_istream_t;
-
-typedef utf_byte_count_t (*utf_read_callback_t)(void * stream_data, void * data, utf_byte_count_t data_max);
+typedef size_t (*utf_read_callback_t)(void * stream_data, void * data, size_t data_max);
 
 struct utf_istream {
-	utf_stream_t stream;
+	struct utf_stream stream;
 	utf_read_callback_t read_cb;
 };
 
-void utf_istream_init(utf_istream_t * istream);
+void utf_istream_init(struct utf_istream * istream);
 
-void utf_istream_free(utf_istream_t * istream);
+void utf_istream_free(struct utf_istream * istream);
 
-utf_codec_t utf_istream_get_codec(const utf_istream_t * istream);
+utf_codec_t utf_istream_get_codec(const struct utf_istream * istream);
 
-void utf_istream_set_codec(utf_istream_t * istream, utf_codec_t codec);
+void utf_istream_set_codec(struct utf_istream * istream, utf_codec_t codec);
 
-void utf_istream_set_data(utf_istream_t * istream, void * data);
+void utf_istream_set_data(struct utf_istream * istream, void * data);
 
-void utf_istream_set_free(utf_istream_t * istream, utf_free_callback_t free_cb);
+void utf_istream_set_free(struct utf_istream * istream, utf_free_callback_t free_cb);
 
-void utf_istream_set_seek(utf_istream_t * istream, utf_seek_callback_t seek_cb);
+void utf_istream_set_seek(struct utf_istream * istream, utf_seek_callback_t seek_cb);
 
-void utf_istream_set_tell(utf_istream_t * istream, utf_tell_callback_t tell_cb);
+void utf_istream_set_tell(struct utf_istream * istream, utf_tell_callback_t tell_cb);
 
-void utf_istream_set_read(utf_istream_t * istream, utf_read_callback_t read_cb);
+void utf_istream_set_read(struct utf_istream * istream, utf_read_callback_t read_cb);
 
-utf_unit_count_t utf_istream_read_utf8(utf_istream_t * istream, utf8_t * data, utf_unit_count_t data_max);
+size_t utf_istream_read(struct utf_istream * istream, struct utf_string * string);
 
-utf_unit_count_t utf_istream_read_utf16(utf_istream_t * istream, utf16_t * data, utf_unit_count_t data_max);
+size_t utf_istream_read_utf8(struct utf_istream * istream, char * data, size_t data_max);
 
-utf_unit_count_t utf_istream_read_utf32(utf_istream_t * istream, utf32_t * data, utf_unit_count_t data_max);
+size_t utf_istream_read_utf16(struct utf_istream * istream, char16_t * data, size_t data_max);
 
-utf_byte_count_t utf_istream_read_bytes(utf_istream_t * istream, void * data, utf_byte_count_t data_max);
+size_t utf_istream_read_utf32(struct utf_istream * istream, char32_t * data, size_t data_max);
+
+size_t utf_istream_read_bytes(struct utf_istream * istream, void * data, size_t data_max);
 
 #endif /* LIBUTF_ISTREAM_H */
 
