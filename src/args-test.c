c42c52f6ba98fc3b3d24d2be87f95ca99b62f317
Taylor Holberton <taylorcholberton@gmail.com>
2017-02-26 11:33:54 -0800

    added utf_args_cmp_utf32() tests

--- src/args-test.c
+++ src/args-test.c
@@ -3,16 +3,21 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 int main(void){
 
 	struct utf_args args;
-	const char * argv[] = { "¿Cómo", "estás?", NULL };
+	const char * argv[] = { u8"¿Cómo", u8"estás?", NULL };
 	int argc = 3;
 
 	assert(utf_args_init(&args, argc, argv) == 0);
 	assert(args.argc == 2);
 	assert(args.argv[2] == NULL);
+
+	assert(utf_args_cmp_utf32(&args, 0, U"¿Cóma") == 1);
+	assert(utf_args_cmp_utf32(&args, 0, U"¿Cómo") == 0);
+	assert(utf_args_cmp_utf32(&args, 0, U"¿Cómz") == -1);
 
 	utf_args_free(&args);
 
