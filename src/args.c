8744e9f052f59782479c27a7f13461d83cee73cc
Taylor Holberton <taylorcholberton@gmail.com>
2017-02-26 11:33:19 -0800

    implemented utf_args_cmp_utf32() and utf_args_count()

--- src/args.c
+++ src/args.c
@@ -1,19 +1,21 @@
 #include <libutf/args.h>
 
 #include <libutf/utf8.h>
+#include <libutf/utf32.h>
 
 #include <errno.h>
 
 int utf_args_init(struct utf_args * args, int argc, const char ** argv){
 
 	int i;
-	size_t argv_size;
 
 	/* the + 1 is for the NULL terminator */
 	args->argv = calloc(sizeof(args->argv[0]), argc + 1);
 	if (args->argv == NULL){
 		return ENOMEM;
 	}
+
+	i = 0;
 
 	for (i = 0; (argv[i] != NULL) && (i < argc); i++){
 		args->argv[i] = utf8_to_utf32(argv[i]);
@@ -41,5 +43,13 @@ void utf_args_free(struct utf_args * args){
 
 	args->argv = NULL;
 	args->argc = 0;
+}
+
+size_t utf_args_count(const struct utf_args * args){
+	return args->argc;
+}
+
+int utf_args_cmp_utf32(const struct utf_args * args, size_t index, const char32_t * arg){
+	return utf32_cmp(args->argv[index], arg);
 }
 
